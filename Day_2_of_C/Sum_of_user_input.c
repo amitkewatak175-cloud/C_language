4ce2bc6567d8ba591e6acf5b0b7d34db5cd56098
Amit <amitkewatak175@gmail.com>
2026-04-11 16:45:10 +0530

    Sum of user input

---
+++ Day_2_of_C/Sum_of_user_input.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+int main()
+{
+    int a, b;
+    printf("Enter the value of A and B.\n");
+    scanf("%d\n%d", &a, &b);
+    printf("Sum of entered value is %d.\n", a+b);
+
+    return 0;
+}
