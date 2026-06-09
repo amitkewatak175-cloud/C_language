6f3acbce3c82943574cd51ec2e18d622f99a04e5
Amit <amitkewatak175@gmail.com>
2026-06-09 19:21:06 +0530

    Practice If statement

---
+++ Day_4_of_C/If_statement.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+int main()
+{
+    int age;
+    printf("Enter your age.\n");
+    scanf("%d", &age);
+    if (age>=18)
+    {
+        printf("You're Eligible for vote.\n");
+    }
+    return 0;
+
+}
